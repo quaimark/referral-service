1a3ec2e556a97cf66b8297611278b58c38568ad5
Trung Do <dttr278@gmail.com>
2024-04-11 14:42:08 +0700

    Fix fee comparison in PointService

--- dist/services/point.service.js
+++ dist/services/point.service.js
@@ -241,7 +241,7 @@ class PointService {
                     txHash: pointHistory.txHash,
                     block: pointHistory.block,
                     chain: pointHistory.chain,
-                    fee: pointHistory.fee,
+                    fee: h.fee,
                     point: refSource.reduce((a, b) => a + b.point, 0),
                     source: refSource,
                     ref: pointHistory.user,
